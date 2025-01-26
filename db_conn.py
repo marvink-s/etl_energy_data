736eaced392751d502b913cb1442099a32ccc216
marvink-s <marvin.klein24@gmx.de>
2025-01-26 13:03:00 +0100

    Updata

--- db_conn.py
+++ db_conn.py
@@ -1,6 +1,11 @@
 import logging
 import sqlalchemy as db
 
+# Configure logging
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
 
 def get_connection():
     """
